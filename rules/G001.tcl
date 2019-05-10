bbd9521e896a543c966e3657ac1476ba4498198a
Clément Beaubestre <clement+hub@beaubestre.fr>
2019-05-10 18:19:45 +0000

    fix G1: no header = 1 error

--- rules/G001.tcl
+++ rules/G001.tcl
@@ -4,29 +4,51 @@
 foreach f [getSourceFileNames] {
     set rule "G1  > $f"
     set lineNumber 1
+    set count 0
 
     foreach line [getAllLines $f] {
 
-        if [regexp {^Makefile$} $f] {
-            if {$lineNumber == 1 && [regexp {^##$} $line]
-            ||  $lineNumber == 2 && ![regexp {^## EPITECH PROJECT, \d{4}$} $line]
-            ||  $lineNumber == 3 && ![regexp {^## .*$} $line]
-            ||  $lineNumber == 4 && ![regexp {^## File description:$} $line]
-            ||  $lineNumber == 5 && ![regexp {^## .*$} $line]
-            ||  $lineNumber == 6 && ![regexp {^##$} $line]} {
-                report $rule $lineNumber "you must start your source code with a correctly formatted Epitech standard header"
-            }
+        if {[regexp {^Makefile$} $f] && [regexp {^##} $line] 
+        ||  $lineNumber == 1 && [regexp {^\/\*} $line]
+        ||  $lineNumber == 6 && [regexp {^\*\/} $line]
+        ||  [regexp {^\*\*} $line] } {
+            incr count
         } else {
-            if {$lineNumber == 1 && ![regexp {^\/\*$} $line]
-            ||  $lineNumber == 2 && ![regexp {^\*\* EPITECH PROJECT, \d{4}$} $line]
-            ||  $lineNumber == 3 && ![regexp {^\*\* .*$} $line]
-            ||  $lineNumber == 4 && ![regexp {^\*\* File description:$} $line]
-            ||  $lineNumber == 5 && ![regexp {^\*\* .*$} $line]
-            ||  $lineNumber == 6 && ![regexp {^\*\/$} $line]} {
-                report $rule $lineNumber "you must start your source code with a correctly formatted Epitech standard header"
-            }
+            break
         }
 
         incr lineNumber
+    }
+
+    if {$count == 6} {
+        set lineNumber 1
+
+        foreach line [getAllLines $f] {
+
+            if [regexp {^Makefile$} $f] {
+                if {$lineNumber == 1 &&  [regexp {^##$} $line]
+                ||  $lineNumber == 2 && ![regexp {^## EPITECH PROJECT, \d{4}$} $line]
+                ||  $lineNumber == 3 && ![regexp {^##.*$} $line]
+                ||  $lineNumber == 4 && ![regexp {^## File description:$} $line]
+                ||  $lineNumber == 5 && ![regexp {^##.*$} $line]
+                ||  $lineNumber == 6 && ![regexp {^##$} $line]} {
+                    report $rule $lineNumber "you must start your source code with a correctly formatted Epitech standard header"
+                }
+            } else {
+                if {$lineNumber == 1 && ![regexp {^\/\*$} $line]
+                ||  $lineNumber == 2 && ![regexp {^\*\* EPITECH PROJECT, \d{4}$} $line]
+                ||  $lineNumber == 3 && ![regexp {^\*\*.*$} $line]
+                ||  $lineNumber == 4 && ![regexp {^\*\* File description:$} $line]
+                ||  $lineNumber == 5 && ![regexp {^\*\*.*$} $line]
+                ||  $lineNumber == 6 && ![regexp {^\*\/$} $line]} {
+                    report $rule $lineNumber "you must start your source code with a correctly formatted Epitech standard header"
+                }
+            }
+
+            incr lineNumber
+        }
+    
+    } else {
+        report $rule 1 "you must start your source code with a correctly formatted Epitech standard header"
     }
 }
